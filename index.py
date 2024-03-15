8d1f4f1b48e66d4e9f817c2bc85adead4d000794
amneet <kauramneet986@gmail.com>
2024-03-15 12:35:09 -0400

    shuffle the numbers

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 # app.py
 # This is a test commit
 # this is for testing GithubActions
+
 def add(a, b):
     return a + b
 
 def test_add():
-    assert add(1, 2) == 3
+    assert add(2, 1) == 3
     assert add(1, -1) == 0
